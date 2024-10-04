c2b4a659adf427ef756a9e64eddd98cd61c8e87f
moaad99 <msellekmoaad@gmail.com>
2024-10-04 21:58:42 +0100

    first commit

---
+++ dist/loader.js
@@ -0,0 +1,38 @@
+// loader.js
+
+(function() {
+    console.log('Loader script is executing');
+  
+    const config = window.ChatbotConfig || {};
+  
+    // Function to load a script
+    function loadScript(src, callback) {
+      const script = document.createElement('script');
+      script.src = src;
+      script.async = true;
+      script.onload = callback;
+      script.onerror = function(error) {
+        console.error('Error loading script:', src, error);
+      };
+      document.head.appendChild(script);
+    }
+  
+    // Load the chatbot script
+    function loadChatbot() {
+      console.log('Loading chatbot script');
+      loadScript('https://cdn.jsdelivr.net/gh/moaad99/AI-Chatbot@v1.0.2/dist/chatbot-widget.js', function() {
+        console.log('Chatbot script loaded');
+  
+        if (window.ChatbotWidget && typeof window.ChatbotWidget === 'function') {
+          console.log('Initializing chatbot with config:', config);
+          window.ChatbotWidget(config);
+        } else {
+          console.error('ChatbotWidget failed to load or is not a function.');
+        }
+      });
+    }
+  
+    // Start loading the chatbot
+    loadChatbot();
+  })();
+  
